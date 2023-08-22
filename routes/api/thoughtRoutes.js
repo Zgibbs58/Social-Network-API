183dd2df3c1f35b201f46ae2501cdae9e1b88168
Zgibbs58 <zacharywgibbs@gmail.com>
2023-08-22 12:36:32 -0500

    Updated the userRoutes to match the route paths needed.

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+const { getCourses, getSingleCourse, createCourse, updateCourse, deleteCourse } = require("../../controllers/courseController.js");
+
+// /api/thoughts
+router.route("/").get(getCourses).post(createCourse);
+
+// /api/courses/:courseId
+router.route("/:courseId").get(getSingleCourse).put(updateCourse).delete(deleteCourse);
+
+module.exports = router;
---
+++ routes/api/userRoutes.js
@@ -0,0 +1,13 @@
+const router = require("express").Router();
+const {} = require("../../controllers/userController");
+
+// /api/users
+router.route("/").get(getUsers).post(createUser);
+
+// /api/users/:userId
+router.route("/:userId").get(getSingleUser).delete(deleteUser).put(updateUser);
+
+// /api/users/:userId/friends/:friendId
+router.route("/:userId/friends/:friendId").post(addFriend).delete(deleteFriend);
+
+module.exports = router;
