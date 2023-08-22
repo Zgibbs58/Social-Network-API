6ba45f2e4e22bc9972602b5da9d7bb8e7134f28b
Zgibbs58 <zacharywgibbs@gmail.com>
2023-08-22 11:01:50 -0500

    Imported file structure and starter code from mini project to adapt to this API. Also, set up the User Model to match the structure needed for the Social Network API.

---
+++ models/User.js
@@ -0,0 +1,49 @@
+const { Schema, model } = require("mongoose");
+
+// Schema to create Student model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator: function (v) {
+          return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(v);
+        },
+        message: (props) => `${props.value} is not a valid email!`,
+      },
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "friend",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+studentSchema.virtual("friendCount").get(function () {
+  return `${this.friends.length} Friends`;
+});
+
+const User = model("user", userSchema);
+
+module.exports = User;
