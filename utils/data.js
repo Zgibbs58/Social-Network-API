c6c0fbccdefe178272708751dd60b5f06576a0d8
Zgibbs58 <zacharywgibbs@gmail.com>
2023-08-22 15:26:50 -0500

    Added data.js functions to randomly pull data from array and added seed.js to loop through and populate that data.

--- utils/data.js
+++ utils/data.js
@@ -1,118 +1,90 @@
-const names = [
-  'Aaran',
-  'Aaren',
-  'Aarez',
-  'Aarman',
-  'Aaron',
-  'Aaron-James',
-  'Aarron',
-  'Aaryan',
-  'Aaryn',
-  'Aayan',
-  'Aazaan',
-  'Abaan',
-  'Abbas',
-  'Abdallah',
-  'Abdalroof',
-  'Abdihakim',
-  'Abdirahman',
-  'Abdisalam',
-  'Abdul',
-  'Abdul-Aziz',
-  'Abdulbasir',
-  'Abdulkadir',
-  'Abdulkarem',
-  'Smith',
-  'Jones',
-  'Coollastname',
-  'enter_name_here',
-  'Ze',
-  'Zechariah',
-  'Zeek',
-  'Zeeshan',
-  'Zeid',
-  'Zein',
-  'Zen',
-  'Zendel',
-  'Zenith',
-  'Zennon',
-  'Zeph',
-  'Zerah',
-  'Zhen',
-  'Zhi',
-  'Zhong',
-  'Zhuo',
-  'Zi',
-  'Zidane',
-  'Zijie',
-  'Zinedine',
-  'Zion',
-  'Zishan',
-  'Ziya',
-  'Ziyaan',
-  'Zohaib',
-  'Zohair',
-  'Zoubaeir',
-  'Zubair',
-  'Zubayr',
-  'Zuriel',
-  'Xander',
-  'Jared',
-  'Courtney',
-  'Gillian',
-  'Clark',
-  'Jared',
-  'Grace',
-  'Kelsey',
-  'Tamar',
-  'Alex',
-  'Mark',
-  'Tamar',
-  'Farish',
-  'Sarah',
-  'Nathaniel',
-  'Parker',
+const usernames = [
+  "blueSky22",
+  "cosmicDancer",
+  "radiantSoul",
+  "moonlightDream",
+  "wonderWander",
+  "electricEcho",
+  "silverArrow",
+  "mysticGrove",
+  "starryEyes",
+  "enchantedWhisper",
+  "sapphireSeas",
+  "dreamWeaver88",
+  "velvetRaven",
+  "shadowWalker",
+  "midnightSerene",
+  "crystalVoyager",
+  "etherealMist",
+  "blazingPhoenix",
+  "aquaSerenity",
+  "emberHeart",
 ];
 
-const appDescriptions = [
-  'Decision Tracker',
-  'Find My Phone',
-  'Learn Piano',
-  'Starbase Defender',
-  'Tower Defense',
-  'Monopoly Money Manager',
-  'Movie trailers',
-  'Hello world',
-  'Stupid Social Media App',
-  'Notes',
-  'Messages',
-  'Email',
-  'Compass',
-  'Firefox',
-  'Running app',
-  'Cooking app',
-  'Poker',
-  'Deliveries',
+const Thoughts = [
+  "Lost in thought, finding my way back.",
+  "If words were colors, my mind would be a painting.",
+  "Chasing dreams in a world of whispers.",
+  "Thoughts like fireflies, flickering in the night.",
+  "Capturing moments with the lens of my mind.",
+  "In the silence, thoughts dance like shadows.",
+  "A tapestry of thoughts woven with starlight.",
+  "Wandering through the corridors of my mind.",
+  "Thoughts are seeds; my mind is the garden.",
+  "Echoes of yesterday's thoughts linger softly.",
+  "My mind, an orchestra of untamed thoughts.",
+  "Exploring the universe within my thoughts.",
+  "Waves of thoughts crashing against the shores of reality.",
+  "Where thoughts lead, the heart follows.",
+  "Thoughts, like constellations, guide my way.",
+];
+
+const reactions = [
+  "I disagree!",
+  "I tried your algorithm, here were the results",
+  "This was awesome",
+  "Thank you for the great content",
+  "Please check out my video response",
+  "Like and subscribe to my channel please",
+  "Reply: The side effects of in app purchases on digital marketplaces",
 ];
 
 // Get a random item given an array
 const getRandomArrItem = (arr) => arr[Math.floor(Math.random() * arr.length)];
 
 // Gets a random full name
-const getRandomName = () =>
-  `${getRandomArrItem(names)} ${getRandomArrItem(names)}`;
+const getRandomName = () => `${getRandomArrItem(usernames)}}`;
 
 // Function to generate random assignments that we can add to student object.
-const getRandomAssignments = (int) => {
+const getRandomThoughts = (int) => {
   const results = [];
   for (let i = 0; i < int; i++) {
     results.push({
-      assignmentName: getRandomArrItem(appDescriptions),
-      score: Math.floor(Math.random() * (99 - 70 + 1) + 70),
+      thoughts: getRandomArrItem(Thoughts),
+    });
+  }
+  return results;
+};
+
+const getRandomFriends = (int) => {
+  const results = [];
+  for (let i = 0; i < int; i++) {
+    results.push({
+      thoughts: getRandomArrItem(usernames),
+    });
+  }
+  return results;
+};
+
+const getRandomrReactions = (int) => {
+  const results = [];
+  for (let i = 0; i < int; i++) {
+    results.push({
+      thoughts: getRandomArrItem(reactions),
     });
   }
   return results;
 };
 
 // Export the functions for use in seed.js
-module.exports = { getRandomName, getRandomAssignments };
+module.exports = { getRandomName, getRandomThoughts, getRandomFriends, getRandomrReactions };
